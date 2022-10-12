9d956698bd790828cccccf809aed49367220b5d2
HashTucE <jeremyb59@wanadoo.fr>
2022-10-12 11:27:31 +0200

    Merge Specific dans Person

--- src/main/java/com/safetynet/alerts/AlertsApplication.java
+++ src/main/java/com/safetynet/alerts/AlertsApplication.java
@@ -1,7 +1,5 @@
 package com.safetynet.alerts;
 
-import com.safetynet.alerts.repository.MedicalRecordRepository;
-import com.safetynet.alerts.util.DataWriter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/safetynet/alerts/controller/PersonController.java
+++ src/main/java/com/safetynet/alerts/controller/PersonController.java
@@ -1,10 +1,13 @@
 package com.safetynet.alerts.controller;
 
+import com.safetynet.alerts.dto.*;
 import com.safetynet.alerts.model.Person;
 import com.safetynet.alerts.service.PersonService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -53,4 +56,52 @@ public class PersonController {
         log.debug("Object founded successfully. Returning status 200 (Ok).");
         return personService.findAllPersons();
     }
+
+
+
+
+
+    @GetMapping("/firestation")
+    public ResponseEntity<FireCoverageByStation> getCoverageListFromStation(@RequestParam Integer stationNumber) {
+        log.debug("Object founded successfully. Returning status 200 (Ok).");
+//        return specificService.getCoverageListFromStation(stationNumber);
+        return new ResponseEntity<FireCoverageByStation>(personService.getCoverageListFromStation(stationNumber), HttpStatus.OK);
+    }
+
+    @GetMapping("/childAlert")
+    public ChildByFamily getChildrenListByAddress(@RequestParam String address) {
+        log.debug("Object founded successfully. Returning status 200 (Ok).");
+        return personService.getChildrenListByAddress(address);
+    }
+
+    @GetMapping("/phoneAlert")
+    public List<String> getPhoneListFromStation(@RequestParam Integer firestation) {
+        log.debug("Object founded successfully. Returning status 200 (Ok).");
+        return personService.getPhoneListFromStation(firestation);
+    }
+
+    @GetMapping("/fire")
+    public FireCoverageByAddress getInhabitantsByAddress(@RequestParam String address) {
+        log.debug("Object founded successfully. Returning status 200 (Ok).");
+        return personService.getInhabitantsByAddress(address);
+    }
+
+    @GetMapping("/flood/stations")
+    public List<FloodCoverage> getInhabitantsByStation(@RequestParam List<Integer> stations) {
+        log.debug("Object founded successfully. Returning status 200 (Ok).");
+        return personService.getInhabitantsByStation(stations);
+    }
+
+    @GetMapping("/personInfo")
+    public List<PersonInfo> findPersonInfoByName(@RequestParam String firstName, @RequestParam String lastName) {
+        log.debug("Object founded successfully. Returning status 200 (Ok).");
+        return personService.findPersonInfoByName(firstName, lastName);
+    }
+
+    @GetMapping("/communityEmail")
+    public List<String> findAllEmailsByCity(@RequestParam String city) {
+        log.debug("Object founded successfully. Returning status 200 (Ok).");
+        return personService.findAllEmailsByCity(city);
+    }
+
 }
